4649a9c084ccb25fa5300bdfc6289bedbefffd0b
user-faiz <email2faizm@gmail.com>
2023-09-02 01:13:34 +0530

    fixed linters error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BookListContainer from './components/BookListContainer';
 import Navigation from './components/navigation';
-import './App.css';
+import './styles/App.css';
 
 function CategoriesPage() {
   return <h2>Categories Page Under Construction</h2>;
