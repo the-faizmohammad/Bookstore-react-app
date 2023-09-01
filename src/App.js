7ec55b4b4308f633b34f63b90f056ff3c736ad3a
user-faiz <email2faizm@gmail.com>
2023-09-02 00:49:26 +0530

    Add Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import BookListContainer from './components/BookListContainer';
+import './App.css'; // Import the CSS file
+
+function CategoriesPage() {
+  return <h2>Categories Page Under Construction</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bookstore</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<BookListContainer />} />
+          <Route path="/categories" element={<CategoriesPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
