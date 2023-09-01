a9859bda9ed88d0a39bb7712fa2dde6c08d3f675
user-faiz <email2faizm@gmail.com>
2023-09-02 00:35:49 +0530

    Add function to display list

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import Book from './Book';
+
+const BookList = ({ books, onDelete }) => {
+  return (
+    <div>
+      <h2>Book List</h2>
+      {books.map((book) => (
+        <Book key={book.id} book={book} onDelete={onDelete} />
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
 
