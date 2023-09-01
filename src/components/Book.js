d647a8e862bfc01b15cba141d51fa27e11b18834
user-faiz <email2faizm@gmail.com>
2023-09-02 00:32:32 +0530

    Add display book content and delete btn

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,1 +1,14 @@
+import React from 'react';
+
+const Book = ({ book, onDelete }) => {
+  return (
+    <div>
+      <p>{book.title}</p>
+      <p>{book.author}</p>
+      <button onClick={() => onDelete(book.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default Book;
 
