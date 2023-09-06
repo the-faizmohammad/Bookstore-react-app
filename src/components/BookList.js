ae71e77d6e0e91dcbbab4d19af831167bbb61447
user-faiz <email2faizm@gmail.com>
2023-09-07 02:37:48 +0530

    useSelector hook inside the BookList function

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,26 +1,19 @@
-import React, { useState } from 'react';
+// /src/components/BookList.js
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
-const BookList = () => {
-  const [books, setBooks] = useState([
-    { id: 1, title: 'Book 1', author: 'Author 1' },
-    { id: 2, title: 'Book 2', author: 'Author 2' },
-    // Add more books here as needed
-  ]);
-
-  const deleteBook = (id) => {
-    const updatedBooks = books.filter((book) => book.id !== id);
-    setBooks(updatedBooks);
-  };
+function BookList() {
+  const books = useSelector((state) => state.books);
 
   return (
     <div>
       <h2>Book List</h2>
       {books.map((book) => (
-        <Book key={book.id} book={book} onDelete={deleteBook} />
+        <Book key={book.item_id} book={book} />
       ))}
     </div>
   );
-};
+}
 
 export default BookList;
