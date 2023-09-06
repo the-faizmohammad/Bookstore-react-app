99143484e7743466678a6c8def0079c5a2dddc7a
user-faiz <email2faizm@gmail.com>
2023-09-07 02:59:28 +0530

    Fixed Linters error

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Book from './Book';
 import PropTypes from 'prop-types';
+import Book from './Book';
 
 function BookList() {
   const books = useSelector((state) => state.books);
@@ -22,7 +22,7 @@ BookList.propTypes = {
       title: PropTypes.string.isRequired,
       author: PropTypes.string.isRequired,
       category: PropTypes.string.isRequired,
-    })
+    }),
   ).isRequired,
 };
 export default BookList;
