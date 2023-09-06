7f5314c9beb81a65d48630dfa20d4dfc4bbd0568
user-faiz <email2faizm@gmail.com>
2023-09-07 02:54:52 +0530

    create function AddBookButton

--- src/components/AddBookButton.js
+++ src/components/AddBookButton.js
@@ -1,3 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/booksSlice';
+function AddBookButton({ title, author }) {
+    const dispatch = useDispatch();
+}
