d0bd18dbda6fbc716d3ba9fada7b52d9c8093d5a
user-faiz <email2faizm@gmail.com>
2023-09-07 02:31:36 +0530

    Define initial state in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,16 @@
-// /src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
 import categoriesReducer from './categories/categoriesSlice';
+
+const initialState = {
+};
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     categories: categoriesReducer,
   },
+  preloadedState: initialState,
 });
 
 export default store;
