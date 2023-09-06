20b96a0e27dc8576954333b0a0932a3c5031daf4
user-faiz <email2faizm@gmail.com>
2023-09-07 00:44:50 +0530

    Add booksslice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -3,3 +3,15 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   books: [], 
 };
+const booksSlice = createSlice({
+    name: 'books',
+    initialState,
+    reducers: {
+      addBook: (state, action) => {
+        state.books.push(action.payload);
+      },
+      removeBook: (state, action) => {
+        state.books = state.books.filter((book) => book.id !== action.payload);
+      },
+    },
+  });
