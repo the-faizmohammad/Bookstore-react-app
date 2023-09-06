85e4f6293fb86043e4a7eb6ba33b9e7d4c6ee168
user-faiz <email2faizm@gmail.com>
2023-09-07 02:56:46 +0530

    add handleAddBook function

--- src/components/AddBookButton.js
+++ src/components/AddBookButton.js
@@ -4,3 +4,13 @@ import { addBook } from '../redux/books/booksSlice';
 function AddBookButton({ title, author }) {
     const dispatch = useDispatch();
 }
+const handleAddBook = () => {
+    const itemId = 'item4';
+    const newBook = {
+      item_id: itemId,
+      title,
+      author,
+      category: 'Fiction', 
+    };
+    dispatch(addBook(newBook));
+  };
