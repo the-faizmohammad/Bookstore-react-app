0bddb82ffdf76bcd1b7721754bf290ba3f3b9f76
user-faiz <email2faizm@gmail.com>
2023-09-07 00:50:01 +0530

    create redux slice

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -3,3 +3,10 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   categories: 'Under construction',
 };
+const categoriesSlice = createSlice({
+    name: 'categories',
+    initialState,
+    reducers: {},
+  });
+  
+  export default categoriesSlice.reducer;
