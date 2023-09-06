3a6e2cc8c2906596393cecf03f39518ceb359cb7
user-faiz <email2faizm@gmail.com>
2023-09-07 02:57:54 +0530

    update AddBookButton.js

--- src/components/AddBookButton.js
+++ src/components/AddBookButton.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/booksSlice';
+
 function AddBookButton({ title, author }) {
-    const dispatch = useDispatch();
-}
-const handleAddBook = () => {
+  const dispatch = useDispatch();
+
+  const handleAddBook = () => {
     const itemId = 'item4';
     const newBook = {
       item_id: itemId,
       title,
       author,
-      category: 'Fiction', 
+      category: 'Fiction',
     };
     dispatch(addBook(newBook));
   };
+
+  return (
+    <button onClick={handleAddBook}>Add Book</button>
+  );
+}
+
+export default AddBookButton;
