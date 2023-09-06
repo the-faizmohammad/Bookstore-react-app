534b2c3cae8265448d0c717ccae08a42649384c0
user-faiz <email2faizm@gmail.com>
2023-09-07 03:17:13 +0530

    Update RemoveBookButton.js

--- src/components/RemoveBookButton.js
+++ src/components/RemoveBookButton.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
+
 function RemoveBookButton({ itemId }) {
-    const dispatch = useDispatch();
-  
-    const handleRemoveBook = () => {
-      dispatch(removeBook(itemId));
-    };
-  
-    return (
-      <button onClick={handleRemoveBook}>Remove Book</button>
-    );
-  }
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = () => {
+    dispatch(removeBook(itemId));
+  };
+
+  return (
+    <button type="button" onClick={handleRemoveBook}>
+      Remove Book
+    </button>
+  );
+}
+
+RemoveBookButton.propTypes = {
+  itemId: PropTypes.string.isRequired,
+};
+
+export default RemoveBookButton;
