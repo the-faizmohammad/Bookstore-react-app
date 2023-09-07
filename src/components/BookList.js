9d51dee8a8e64fc3683b788e68f69acfc0335eb4
user-faiz <email2faizm@gmail.com>
2023-09-08 04:02:37 +0530

    add useFFect conditions

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -7,6 +7,15 @@ const BookList = () => {
   const dispatch = useDispatch();
   const books = useSelector((state) => state.books.books);
   const status = useSelector((state) => state.books.status);
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchBooks());
+    }
+  }, [status, dispatch]);
+
+  const handleRemoveBook = (item_id) => {
+    dispatch(removeBook(item_id));
+  };
   return (
     <div className="book-list">
       {books.map((book) => (
