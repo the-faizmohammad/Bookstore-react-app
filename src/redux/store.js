d0917f54df6f2e491cef8d9396d7dc37ab44eedb
user-faiz <email2faizm@gmail.com>
2023-09-08 01:27:27 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,36 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
 import categoriesReducer from './categories/categoriesSlice';
 
-const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
-  categories: [],
-};
-
 const store = configureStore({
   reducer: {
     books: booksReducer,
     categories: categoriesReducer,
   },
-  preloadedState: initialState,
 });
 
 export default store;
