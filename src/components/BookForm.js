744600d2f3312a8a9d295dd8d88b1ae84c00be16
user-faiz <email2faizm@gmail.com>
2023-09-08 01:20:49 +0530

    Update BookForm

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,50 +1,41 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-const BookForm = ({ onSubmit }) => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+const Book = ({ book }) => {
+  const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Check if onSubmit is a function before calling it
-    if (typeof onSubmit === 'function') {
-      onSubmit({ title, author });
-    }
-    setTitle('');
-    setAuthor('');
+  const handleRemoveBook = () => {
+    dispatch(removeBook(book.item_id));
   };
 
   return (
     <div>
-      <h2>Add a New Book</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Author"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <button type="submit">Add</button>
-      </form>
+      <h3>{book.title}</h3>
+      <p>
+        Author:
+        {book.author}
+      </p>
+      <p>
+        Category:
+        {book.category}
+      </p>
+      <button type="button" onClick={handleRemoveBook}>
+        Remove
+      </button>
     </div>
   );
 };
 
-// Define PropTypes for the onSubmit prop
-BookForm.propTypes = {
-  onSubmit: PropTypes.func,
+// Define PropTypes for the 'book' prop
+Book.propTypes = {
+  book: PropTypes.shape({
+    item_id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
-// Add defaultProps for onSubmit
-BookForm.defaultProps = {
-  onSubmit: null, // You can set a default value if needed
-};
-
-export default BookForm;
+export default Book;
