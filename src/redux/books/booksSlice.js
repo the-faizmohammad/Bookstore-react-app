4681e2252bfb99fe450c1781deb2eb428380c856
user-faiz <email2faizm@gmail.com>
2023-09-08 03:52:08 +0530

    Define Async Thunk to Add books from API

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -12,7 +12,14 @@ export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
   );
   return response.data;
 });
-
+// Define an async thunk for adding a book to the API
+export const addBook = createAsyncThunk('books/addBook', async (bookData) => {
+  const response = await axios.post(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/sZfsUrZIdaGOBVYN2H6F/books',
+    bookData
+  );
+  return response.data;
+});
 const booksSlice = createSlice({
   name: 'books',
   initialState,
