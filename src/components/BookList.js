ff5b03a312c6cad3bad408adf307a78eefb18dd3
user-faiz <email2faizm@gmail.com>
2023-09-08 03:59:47 +0530

    Delete Uncesessary comments

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,14 +4,11 @@ import { useSelector } from 'react-redux';
 import Book from './Book';
 
 const BookList = () => {
-  // Assuming you have a books slice in your Redux state
   const books = useSelector((state) => state.books);
 
   return (
     <div className="book-list">
       {books.map((book) => (
-        // Ensure that 'book' object has required properties
-        // and provide a unique 'key' prop
         <Book key={book.item_id} book={book} />
       ))}
     </div>
