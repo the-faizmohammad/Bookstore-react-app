1bde47d75ecf5bcb04a3a57fa9d181fd4e4522b5
user-faiz <email2faizm@gmail.com>
2023-09-08 04:01:18 +0530

    initialise dispatch,books,status

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,12 @@
-// src/components/BookList.js
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBooks, removeBook } from '../redux/books/booksSlice';
 import Book from './Book';
 
 const BookList = () => {
-  const books = useSelector((state) => state.books);
-
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books.books);
+  const status = useSelector((state) => state.books.status);
   return (
     <div className="book-list">
       {books.map((book) => (
